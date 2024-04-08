9ef958d369f69b4453028335658608293cce2aed
roshan142 <72061555+roshan142@users.noreply.github.com>
2024-04-08 22:30:16 +0530

    Add files via upload

---
+++ Meme_Website/main.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template 
+import requests
+import json
+
+app = Flask(__name__)
+app.config['secret_key'] = "RoshanRaja"
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET",url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+@app.route('/')
+def index():
+    meme_pic , subreddit = get_meme()
+    return render_template("index.html",meme_pic=meme_pic, subreddit=subreddit)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=80,debug=True)
